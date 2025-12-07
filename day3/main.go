c1e3485322799de5ba9b5931bd0c5c8f314db0a2
j <chroma@chroma.zone>
2025-12-06 20:30:21 -0800

    Day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -6,28 +6,31 @@ import (
 	"os"
 )
 
+const part2 = true
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		firstIdx := -1
-		firstDigit := byte(0)
-		for i, end := 0, len(line); i < end-1; i++ {
-			digit := line[i] - '0'
-			if digit > firstDigit {
-				firstIdx = i
-				firstDigit = digit
-			}
+		lastDigitIdx := -1
+		jolts := 0
+		battCount := 2
+		if part2 {
+			battCount = 12
 		}
-		secondDigit := byte(0)
-		for i, end := firstIdx+1, len(line); i < end; i++ {
-			digit := line[i] - '0'
-			if digit > secondDigit {
-				secondDigit = digit
+		for ; battCount > 0; battCount-- {
+			maxDigit := byte(0)
+			for i, end := lastDigitIdx+1, len(line); i < end-battCount+1; i++ {
+				digit := line[i] - '0'
+				if digit > maxDigit {
+					lastDigitIdx = i
+					maxDigit = digit
+				}
 			}
+			jolts = jolts*10 + int(maxDigit)
 		}
-		sum += int(firstDigit*10 + secondDigit)
+		sum += jolts
 	}
 	fmt.Println(sum)
 }
